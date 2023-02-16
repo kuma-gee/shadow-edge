684d18236eb4436fdad341486e4cd12c501ffd68
myin <minyin142p@gmail.com>
2023-02-16 17:22:52 +0100

    show arrow on bow

--- src/player/bow.gd
+++ src/player/bow.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var arrow_scene: PackedScene
 
 @onready var firerate_timer: Timer = $FireRate
+@onready var arrow_sprite: Sprite2D = $Arrow
 
 var can_fire = true
 
@@ -14,8 +15,10 @@ func fire():
 	arrow.global_position = global_position
 	get_tree().current_scene.add_child(arrow)
 	can_fire = false
+	arrow_sprite.hide()
 	firerate_timer.start()
 
 
 func _on_fire_rate_timeout():
 	can_fire = true
+	arrow_sprite.show()
