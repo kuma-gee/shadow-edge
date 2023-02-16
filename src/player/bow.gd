714f0e044daeb54050df00b0e450199cc788e615
myin <minyin142p@gmail.com>
2023-02-16 16:54:26 +0100

    add firerate

--- src/player/bow.gd
+++ src/player/bow.gd
@@ -2,8 +2,20 @@ extends Node2D
 
 @export var arrow_scene: PackedScene
 
+@onready var firerate_timer: Timer = $FireRate
+
+var can_fire = true
+
 func fire():
+	if not can_fire: return
+	
 	var arrow = arrow_scene.instantiate()
 	arrow.global_rotation = global_rotation
 	arrow.global_position = global_position
 	get_tree().current_scene.add_child(arrow)
+	can_fire = false
+	firerate_timer.start()
+
+
+func _on_fire_rate_timeout():
+	can_fire = true
